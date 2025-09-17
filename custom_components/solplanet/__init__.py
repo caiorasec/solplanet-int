35e36dd95d3551004d50915885a21a969e5274e8
Caio Silva <cainho.rasec@gmail.com>
2025-09-17 15:41:48 -0300

    Atualização Token

--- custom_components/solplanet/__init__.py
+++ custom_components/solplanet/__init__.py
@@ -1,11 +1,37 @@
+from datetime import timedelta
+from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN, PLATFORMS
+from .api import SolplanetApi
+import aiohttp
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
+    session = aiohttp.ClientSession()
+    api = SolplanetApi(entry.data["username"], entry.data["password"], session)
+    await api.login()
+
+    hass.data[DOMAIN][entry.entry_id] = {
+        "api": api,
+        "plant_id": entry.data["plant_id"],
+    }
+
+    # 🔄 Atualiza o token a cada 4 horas
+    async def refresh_token(now):
+        try:
+            await api.login()
+        except Exception as e:
+            hass.logger.warning("Falha ao renovar token Solplanet: %s", e)
+
+    async_track_time_interval(hass, refresh_token, timedelta(hours=4))
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
--- custom_components/solplanet/api.py
+++ custom_components/solplanet/api.py
@@ -8,23 +8,26 @@ class SolplanetApi:
         self._token = None
 
     async def login(self):
-        url = "https://account.solplanet.net/api/oauth/token"
-        payload = {
-            "username": self._username,
-            "password": self._password,
-            "grant_type": "password"
-        }
-        async with self._session.post(url, data=payload) as resp:
+        """Autentica na Solplanet e guarda o token"""
+        url = f"https://internation-cloud.solplanet.net/api/user/login?account={self._username}&password={self._password}"
+        async with self._session.post(url) as resp:
             data = await resp.json()
-            self._token = data.get("access_token")
+            if "data" not in data or "token" not in data["data"]:
+                raise Exception(f"Falha no login: {data}")
+            self._token = data["data"]["token"]
         return self._token
 
     async def fetch_inverter(self, plant_id: str):
+        """Busca dados do inversor usando token válido"""
+        if not self._token:
+            await self.login()
+
         url = f"https://internation-cloud.solplanet.net/api/plant/invList?current=1&pageSize=10&plantId={plant_id}"
         headers = {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "token": self._token
+            "token": self._token,
+            "Cookie": f"apitoken={self._token}"
         }
         async with self._session.get(url, headers=headers) as resp:
             return await resp.json()
