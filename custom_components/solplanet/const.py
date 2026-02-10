6e4b33f88dfe84a1884cbfaa201b63b79b276aa9
Caio Silva <cainho.rasec@gmail.com>
2026-02-10 13:14:48 -0300

    Fix config flow error mapping and add translation strings

--- custom_components/solplanet/const.py
+++ custom_components/solplanet/const.py
@@ -1,3 +1,9 @@
 DOMAIN = "solplanet"
 PLATFORMS = ["sensor"]
 API_BASE = "https://internation-cloud.solplanet.net/api"
+
+CONF_PLANT_ID = "plant_id"
+CONF_TOKEN = "token"
+CONF_APITOKEN = "apitoken"
+
+COORDINATOR = "coordinator"
